dbd7897e70d849287cfcc596ad44406766e63a55
Mikhail Advani <mikhail.advani@gmail.com>
2016-11-03 16:56:59 +0530

    Made test execution conditional based on config file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import unittest
+import yaml
+import sys
 from networking import NetworkingLevel1
 
-if __name__ == '__main__':
-    unittest.main()
+suite = unittest.TestSuite()
+file = open(sys.argv[1], 'r')
+testConfig = yaml.load(file)
+
+for test, enabled in testConfig['networkingLevel1'].iteritems():
+    if enabled:
+        suite.addTest(NetworkingLevel1(test))
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
