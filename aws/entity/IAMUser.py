fabf5675cb61f229588bb167e66a471b39196839
Mikhail Advani <mikhail.advani@gmail.com>
2016-11-04 18:42:29 +0530

    Check for users not logged in since 90 days

--- aws/entity/IAMUser.py
+++ aws/entity/IAMUser.py
@@ -1,4 +1,25 @@
+from dateutil.parser import parse
+import datetime
+
 class IAMUser():
     def __init__(self, userDict):
         self.user = userDict['user']
         self.mfa = userDict['mfa_active'] == 'true'
+        self.passwordEnabled = userDict['password_enabled'] != 'false'
+        self.passwordLastUsed = userDict['password_last_used']
+
+    def mfaEnabled(self):
+        return (not self.passwordEnabled) | self.mfa
+
+    def credentialsUsed(self, days):
+        if self.passwordEnabled:
+            return self._daysPassed(self.passwordLastUsed) <= days
+        else:
+            return True
+
+    def _daysPassed(self, dateStr):
+        passwordLastUsedDate = parse(dateStr)
+        date = datetime.date(passwordLastUsedDate.year, passwordLastUsedDate.month, passwordLastUsedDate.day)
+        now = datetime.datetime.now()
+        today = datetime.date(now.year, now.month, now.day)
+        return (today - date).days
--- iam/IamLevel1.py
+++ iam/IamLevel1.py
@@ -5,12 +5,26 @@ from aws.entity import IAMUser
 class IamLevel1(unittest.TestCase):
     def testMfaEnabledForConsoleUsers(self):
         iamUsersWithoutMfa = []
+        for iamUser in self._getIamUserList():
+            if not iamUser.mfaEnabled():
+                iamUsersWithoutMfa.append(iamUser)
+        self.assertEqual([], iamUsersWithoutMfa, "Users %s have console passwords without MFA" % self._users(iamUsersWithoutMfa))
+
+    def testUnusedCredentialsAreDeactivated(self):
+        oldUserTimePeriod = 90
+        iamUsersWithUnusedCredentials = []
+        for iamUser in self._getIamUserList():
+            if not iamUser.credentialsUsed(oldUserTimePeriod):
+                iamUsersWithUnusedCredentials.append(iamUser)
+        self.assertEqual([], iamUsersWithUnusedCredentials, "Active users %s have passwords unused for long" % self._users(iamUsersWithUnusedCredentials))
+
+    def _getIamUserList(self):
+        iamUserList = []
         users = IAM().getCredentialReport()
         for user in users:
             iamUser = IAMUser(user)
-            if not iamUser.mfa:
-                iamUsersWithoutMfa.append(iamUser)
-        self.assertEqual([], iamUsersWithoutMfa, "Users %s have console passwords without MFA" % self._users(iamUsersWithoutMfa))
+            iamUserList.append(iamUser)
+        return iamUserList
 
     def _users(self, iamUsers):
         users = []
