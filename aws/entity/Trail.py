337971d267052fac2dde52cbefd225e2410b0ae7
Mikhail Advani <mikhail.advani@gmail.com>
2016-11-13 19:57:11 +0530

    Check validation in enabled for cloudtrail logs

--- aws/entity/Trail.py
+++ aws/entity/Trail.py
@@ -1,3 +1,5 @@
 class Trail():
     def __init__(self, cloudTrailDict):
         self.isMultiRegionTrail = cloudTrailDict['IsMultiRegionTrail']
+        self.logFileValidationEnabled = cloudTrailDict['LogFileValidationEnabled']
+        self.name = cloudTrailDict['Name']
--- tests/log/LoggingLevel1.py
+++ tests/log/LoggingLevel1.py
@@ -5,6 +5,25 @@ from aws.entity import Trail
 class LoggingLevel1(unittest.TestCase):
     def testCloudtrailEnabledForAllRegions(self):
         trailEnabledForAllRegions = False
-        for cloudTrail in CloudTrail().getTrails()['trailList']:
-            trailEnabledForAllRegions = trailEnabledForAllRegions | Trail(cloudTrail).isMultiRegionTrail
+        for trail in self._getTrails():
+            trailEnabledForAllRegions = trailEnabledForAllRegions | trail.isMultiRegionTrail
         self.assertEqual(trailEnabledForAllRegions, True, "No multi-region trail defined")
+
+    def testCloudTrailValidationIsEnabled(self):
+        trailsWithValidationDisabled = []
+        for trail in self._getTrails():
+            if not trail.logFileValidationEnabled:
+                trailsWithValidationDisabled.append(trail)
+        self.assertEqual([], trailsWithValidationDisabled, "Trail(s) with validation disabled: %s" % self._trails(trailsWithValidationDisabled))
+
+    def _getTrails(self):
+        trails = []
+        for cloudTrail in CloudTrail().getTrails()['trailList']:
+            trails.append(Trail(cloudTrail))
+        return trails
+
+    def _trails(self, cloudtrails):
+        trails = []
+        for cloudtrail in cloudtrails:
+            trails.append(cloudtrail.name)
+        return ",".join(trails)
