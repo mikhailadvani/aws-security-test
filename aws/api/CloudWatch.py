5042e00ccd03120d2fdefa3bc2103fdafbb60a0f
Mikhail Advani <mikhail.advani@gmail.com>
2016-12-23 17:46:03 +0530

    Throttled calls for DescribeAlarmsForMetric

--- aws/api/CloudWatch.py
+++ aws/api/CloudWatch.py
@@ -1,8 +1,19 @@
 import boto3
-
+import time
 class CloudWatch:
     def __init__(self):
         self.cloudWatch = boto3.client('cloudwatch')
 
     def getAlarms(self, metricName, namespace):
-        return self.cloudWatch.describe_alarms_for_metric(MetricName=metricName, Namespace=namespace)
+        return self._getAlarms(metricName, namespace, 1)
+
+    def _getAlarms(self, metricName, namespace, sleepForIfFailed):
+        alarms = []
+        if sleepForIfFailed > 60:
+            raise RuntimeError("Too many client errors")
+        try:
+            alarms = self.cloudWatch.describe_alarms_for_metric(MetricName=metricName, Namespace=namespace)
+        except ClientError:
+            time.sleep(sleepForIfFailed)
+            sleepForIfFailed(metricName, namespace, sleepForIfFailed * 2)
+        return alarms
